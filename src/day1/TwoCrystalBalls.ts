1c74121406e24c541d37707fd7f4b6660ff95a61
Krasyo <doanusho@gmail.com>
2023-08-05 19:20:30 +0100

    twoCrystalBalls with binary search

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,11 +1,10 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
   const jumpAmount = Math.floor(Math.sqrt(breaks.length));
   let i = jumpAmount;
 
   for (; i < breaks.length; i += jumpAmount) {
-    if (breaks[i]) {
-      break;
+  if (breaks[i]) {
+    break;
     }
   }
 
@@ -14,8 +13,28 @@ export default function two_crystal_balls(breaks: boolean[]): number {
   for (let j = lastJumpPos; j <= i ; j++) {
     if (breaks[j]) {
       return j;
+     }
+   }
+
+   return -1;
+}
+
+
+
+export function two_crystal_balls_binary_search(breaks: boolean[]): number {
+  let left = 0;
+  let right = breaks.length - 1;
+
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (breaks[mid]) {
+      right = mid;
+    } else {
+      left = mid + 1;
     }
   }
 
-  return -1;
+  return breaks[left] ? left : -1;
 }
+

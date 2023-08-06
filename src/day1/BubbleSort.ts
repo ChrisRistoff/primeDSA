8a7a5ba72367b5533a860fc8b2df11f4321a92e3
Krasyo <doanusho@gmail.com>
2023-08-06 14:59:32 +0100

    .

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,12 +1,28 @@
-import quick_sort from "./QuickSort";
-
 export default function bubble_sort(arr: number[]): void {
   let len = arr.length;
 
   for (let i = 0; i < len; i++) {
+    len--;
     for (let j = 0; j < len; j++) {
 
       if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
+
+
+export function reverse_bubble_sort(arr: number[]): void {
+  let len = arr.length;
+
+  for (let i = 0; i < len; i++) {
+    len--;
+    for (let j = 0; j < len; j++) {
+
+      if (arr[j] < arr[j + 1]) {
         const temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
